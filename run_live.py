e08ceaa6e48238820dac801aec7d464325dc016b
zjnzn <l7200000@126.com>
2026-05-28 16:26:55 +0800

    INFO

--- run_live.py
+++ run_live.py
@@ -22,7 +22,7 @@ if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s %(levelname)-5s %(name)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
